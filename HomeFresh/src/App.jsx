f6e0bcf2c0c08829e8a05f2a1cb0e4882633ab2b
Aditya Naresh <adityanaresh007@gmail.com>
2024-03-09 15:50:44 +0530

    Login Page

--- HomeFresh/src/App.jsx
+++ HomeFresh/src/App.jsx
@@ -1,19 +1,26 @@
 import 'tailwindcss/tailwind.css';
 import Nav from './Components/Nav';
 import Home from './pages/Home';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Login from './pages/Login';
 
 function App() {
 
  
 
   return (
-    <>
+    <Router>
+
       <Nav />
-      < Home />
+      <Routes>
+        <Route path = "/"  element = {<Home />} />
+        <Route path = "/login"  element = {<Login />} />
+
+      </Routes>
 
 
       
-    </>
+    </Router>
   )
 }
 
